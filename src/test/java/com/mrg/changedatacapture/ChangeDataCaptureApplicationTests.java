0f6d732d623efe6ae5b2bec7cdc59c718ca816e1
mrgenco <mrgenco@gmail.com>
2023-07-29 07:24:59 +0300

    initial commit

---
+++ src/test/java/com/mrg/changedatacapture/ChangeDataCaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrg.changedatacapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeDataCaptureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
